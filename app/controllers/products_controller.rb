b8a4aeafd6c9fe92171ab86f3a52b09884051d0b
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-11-23 17:40:31 +0100

    product controller set up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,48 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only %i[edit update show destroy]
+  def index
+    @products = Product.all
+  end
+
+  def show;end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product.params)
+    authorize @product
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    authorize @product
+  end
+
+  def update
+    @product.update(product_params)
+    authorize product
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product.destroy
+    authorize @product
+    redirect_to rooth_path
+  end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :category, :amount, :category, :photo)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  belongs_to :category
 end
